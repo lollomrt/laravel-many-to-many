b78f9153b0605a2bbe610924d575157d07ddfd07
lollomrt <lorenzomartini.mrt@outlook.it>
2023-03-06 23:51:51 +0100

    destroy fatta

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -118,6 +118,8 @@ class ProjectController extends Controller
      */
     public function destroy(Project $project)
     {
+        $project->technologies()->sync([]);
+
         $project->delete();
         return redirect()->route('admin.projects.index')->with('message', 'Progetto eliminato con successo');
     }
